67a16eb60de0b7523f3b5c030e1ee98341d022f3
Alphabrandy <alphayohanna2023@gmail.com>
2024-02-12 04:33:24 -0800

    classes inherite from basemodel updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,56 @@
 #!/usr/bin/python3
-"""Base Model"""
-
+"""This script is the base model"""
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """
-    BaseModel class that defines all common attributes/method for other classes
-    """
+
+    """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-        """Initialiazation of the BaseModel Object"""
+        """Initializes instance attributes
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
 
-                elif key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
-
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, key, value)
-
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
-    def save(self):
-        """Update the current datetime"""
-
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        """Change an instance to dictionary"""
-
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-
-        return inst_dict
+            storage.new(self)
 
     def __str__(self):
-        """Return the string representation of the object"""
+        """Returns official string representation"""
 
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
