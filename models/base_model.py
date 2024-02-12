7d9868ba6e17c6dd57d54a5c4b38bb58e41afe83
Saviour Assandoh <asandohs@gmail.com>
2024-02-11 19:35:23 -0800

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,7 @@
 
 import uuid
 from datetime import datetime
-
-
+import models
 class BaseModel:
     """
     BaseModel class that defines all common attributes/method for other classes
@@ -30,11 +29,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """Update the current datetime"""
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Change an instance to dictionary"""
